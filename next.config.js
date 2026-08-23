faa4fa4c7b5dd360f89aa9a7119280c32dfd31a9
Gigi1984P <your-email@example.com>
2026-08-23 16:58:33 +0000

    Add webpack alias for @/ to src/ in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const path = require('path');
+
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
+};
 
 module.exports = nextConfig;
